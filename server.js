eb9916dd27179c11b559dc67e906efd32f2893f3
adarsh-7804 <mradarshpate993l@gmail.com>
2026-04-30 13:18:33 +0530

    Fix final backend CORS and startup

--- server.js
+++ server.js
@@ -31,22 +31,32 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const server = http.createServer(app)
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  process.env.CLIENT_URL
-];
 
 
 
 app.set("etag", false);
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://be-social-frontend-five.vercel.app"
+];
+
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-
+app.options(/.*/, cors());
 
 const io = new Server(server, {
   cors: {

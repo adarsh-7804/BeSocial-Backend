ed206cc9ef046aeb714524bd9c4116636ec17eb8
adarsh-7804 <mradarshpate993l@gmail.com>
2026-05-07 15:26:38 +0530

    Added Optimization of image directly to cloudinary & added fetching all comment in simgle query

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -56,24 +56,55 @@ async function enrichPosts(posts, userId) {
     userReactionMap[like.post.toString()] = like.type;
   });
 
-  return Promise.all(
-    posts.map(async (post) => {
+  // return Promise.all(
+    // posts.map(async (post) => {
       // Handle both Mongoose documents and aggregate results
-      const postObj = post.toObject ? post.toObject() : { ...post };
-      const postId = postObj._id;
+      // const postObj = post.toObject ? post.toObject() : { ...post };
+      // const postId = postObj._id;
+// 
+      // const comments = await Comment.find({ post: postId })
+        // .populate("user", "firstName lastName avatar")
+        // .populate("replies.user", "firstName lastName avatar")
+        // .sort({ createdAt: -1 });
+// 
+      // return {
+        // ...postObj,
+        // comments: comments || [],
+        // currentUserReaction: userReactionMap[postId.toString()] || null,
+      // };
+    // }),
+  // );
 
-      const comments = await Comment.find({ post: postId })
-        .populate("user", "firstName lastName avatar")
-        .populate("replies.user", "firstName lastName avatar")
-        .sort({ createdAt: -1 });
+  const comment = await Comment.find({ 
+    post: { $in: postIds },    
+  })
+  .populate("user", "firstName lastName avatar")
+  .populate("replies.user", "firstName lastName avatar")
+  .sort({ createdAt: -1 });
 
-      return {
-        ...postObj,
-        comments: comments || [],
-        currentUserReaction: userReactionMap[postId.toString()] || null,
-      };
-    }),
-  );
+
+  const commentsMap = {};
+
+  comment.forEach((comment) => {
+    const postId = comment.post.toString();
+  
+    if (!commentsMap[postId]) {
+      commentsMap[postId] =[];
+    }
+    
+    commentsMap[postId].push(comment);
+  })
+
+  return posts.map((post) => {
+    const postObj = post.toObject ? post.toObject() : {...post};
+    const postId = postObj._id.toString();
+
+    return {
+      ...postObj,
+      comments: commentsMap[postId] || [],
+      currentUserReaction: userReactionMap[postId] || null,
+    }
+  })
 }
 
 
